fec8f0356f493d98ab1cd5261e4f89f46e577046
Kasperi Vaarala <n1vaka00@students.oamk.fi>
2022-01-25 09:12:24 +0200

    eka versio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [paino, setPaino] = useState(0);
+  const [intensiteetti, setIntensiteetti] = useState(0);
+  const [sukupuoli, setSukupuoli] = useState("mies");
+  const [tulos, setTulos] = useState(0);
+
+  function laske(e) {
+    e.preventDefault();
+    let kalorit = 0;
+    if (sukupuoli === 'mies') {
+      kalorit = (879 + 10.2 * paino) * intensiteetti
+    } else { 
+      kalorit = (795 + 7.18 * paino) * intensiteetti
+
+    }
+    setTulos(kalorit)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <form>
+     <div>
+     <label> Paino</label>
+     <input type="number" value={paino} onChange={e => setPaino(e.target.value)} ></input>
+     </div>
+     <div>
+       <label>Intensiteettii</label>
+       <select onChange={e => setIntensiteetti(e.target.value)}>
+         <option value="1.3">Kevyt</option>
+         <option value="1.5">Tavallinen</option>
+         <option value="1.7">kohtuullinen</option>
+         <option value="2">kova</option>
+         <option value="2.2">erittäin kova</option>
+
+       </select>
+     </div>
+     <div>
+       <label>Sukupuoli</label>
+       <label> <input type="radio" name="sukupuoli" value="mies" onChange={e => setSukupuoli(e.target.value)} defaultChecked>Mies</input></label>
+       <label> <input type="radio" name="sukupuoli" value="nainen" >Nainen</input></label>
+     </div>
+     <div>
+       <label>Kalorit</label>
+       <output>{tulos.toFixed(0)}</output>
+     </div>
+     <button type="button" onClick={laske}>Laske</button>
+   </form>
   );
 }
 
